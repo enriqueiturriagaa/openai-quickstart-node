5666dc7fbb25a11ebbea8223c4b807771a412b01
enrique.zone <enriqueiturriagaa@gmail.com>
2022-12-14 10:36:35 -0400

    Initial Commit

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,4 +1,5 @@
 import { Configuration, OpenAIApi } from "openai";
+import { writeFileSync } from "fs";
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -6,23 +7,43 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 export default async function (req, res) {
-  const completion = await openai.createCompletion({
-    model: "text-davinci-002",
-    prompt: generatePrompt(req.body.animal),
-    temperature: 0.6,
+  const result = await openai.createImage({
+    prompt: req.body.animal,
+    n: 1,
+    size: "1024x1024",
   });
-  res.status(200).json({ result: completion.data.choices[0].text });
+
+  const url = result.data.data[0].url;
+  console.log(url);
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
+// // save image to disk
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
-}
+// const imageResult = await fetch(url);
+// const blob = await imageResult.blob();
+// const buffer = Buffer.from(await blob.arrayBuffer());
+// writeFileSync(`./img/${Date.now()}.png`, buffer);
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// export default async function (req, res) {
+//   const completion = await openai.createCompletion({
+//     model: "text-davinci-002",
+//     prompt: generatePrompt(req.body.animal),
+//     temperature: 0.6,
+//   });
+//   res.status(200).json({ result: completion.data.choices[0].text });
+// }
+
+// function generatePrompt(animal) {
+//   const capitalizedAnimal =
+//     animal[0].toUpperCase() + animal.slice(1).toLowerCase();
+//   return `Suggest three names for an animal that is a superhero.
+
+// Animal: Cat
+// Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+// Animal: Dog
+// Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+// Animal: ${capitalizedAnimal}
+// Names:`;
+// }
--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,54 @@
-import Head from "next/head";
 import { useState } from "react";
-import styles from "./index.module.css";
+import { Configuration, OpenAIApi } from "openai";
 
-export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
-  const [result, setResult] = useState();
+function App() {
+  const [prompt, setPrompt] = useState("");
+  const [result, setResult] = useState("");
+  const [loader, setloader] = useState("");
 
-  async function onSubmit(event) {
-    event.preventDefault();
-    const response = await fetch("/api/generate", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ animal: animalInput }),
+  const configuration = new Configuration({
+    apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const generateImage = async () => {
+    setloader("Wait while we generate your image!");
+    const res = await openai.createImage({
+      prompt: prompt,
+      n: 1,
+      size: "512x512",
     });
-    const data = await response.json();
-    setResult(data.result);
-    setAnimalInput("");
-  }
+
+    setResult(res.data.data[0].url);
+    setloader(" ");
+  };
 
   return (
-    <div>
-      <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
-      </Head>
+    <div className="flex-wrapper">
+      <>
+        <h2 className="h2-primary">Generate an Image using Open AI API</h2>
 
-      <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
-        <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
-          />
-          <input type="submit" value="Generate names" />
-        </form>
-        <div className={styles.result}>{result}</div>
-      </main>
+        <textarea
+          className="textarea"
+          placeholder="Search Bears with Paint Brushes the Starry Night, painted by Vincent Van Gogh.."
+          onChange={(e) => setPrompt(e.target.value)}
+          rows="5"
+          cols="40"
+        />
+        <br />
+        <button className="btn" onClick={generateImage}>
+          Generate an Image
+        </button>
+        <br />
+        {result.length > 0 ? (
+          <img className="mt-6 w-350" src={result} alt="result" />
+        ) : (
+          <></>
+        )}
+        <p>{loader}</p>
+      </>
     </div>
   );
 }
+
+export default App;
